3d0d3c82c9484b902740559133ec8a41f3d2c739
Mopharr <olamiquadri1@gmail.com>
2023-06-14 15:08:58 +0100

    complete authentication

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,48 @@
 import React from "react";
+import axios from "axios";
+// import { BiErrorCircle } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 import { Flex } from "@chakra-ui/react";
 import "../Style/styles.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const timer = useRef();
+  const [err, setErr] = useState(false);
+  const [sign, setSign] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setSign({ ...sign, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/api/login", {
+        email: sign.email,
+        password: sign.password,
+      })
+      .then((res) => {
+        setSign({
+          email: "",
+          password: "",
+        });
+        setErr(false);
+        navigate("/converter");
+      })
+      .catch((err) => {
+        console.log(err);
+        setErr(true);
+        timer.current = window.setTimeout(() => {
+          setErr(false);
+        }, 4000);
+      });
+  };
+
   return (
     <>
       <Flex
@@ -17,9 +57,19 @@ const Login = () => {
         <div className="body">
           <h1>Login</h1>
 
-          <form>
-            <input type="email" placeholder="Enter Email" />
-            <input type="password" placeholder="**********" />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              value={sign.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="**********"
+              value={sign.password}
+              onChange={handleChange}
+            />
             <button type="submit">Login</button>
           </form>
         </div>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
+import axios from "axios";
+import { BiErrorCircle } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
+
 import "../Style/styles.css";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const timer = useRef();
+  const [err, setErr] = useState(false);
+  const [sign, setSign] = useState({
+    fullName: "",
+    userName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setSign({ ...sign, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/api/register", {
+        fullName: sign.fullName,
+        userName: sign.userName,
+        email: sign.email,
+        password: sign.password,
+      })
+      .then((res) => {
+        setSign({
+          fullName: "",
+          userName: "",
+          email: "",
+          password: "",
+        });
+        setErr(false);
+        navigate("/converter");
+      })
+      .catch((err) => {
+        console.log(err);
+        setErr(true);
+        timer.current = window.setTimeout(() => {
+          setErr(false);
+        }, 4000);
+      });
+  };
+
   return (
     <>
       <Flex
@@ -17,11 +64,34 @@ const SignUp = () => {
         <div className="body">
           <h1>Sign Up</h1>
 
-          <form>
-            <input type="email" placeholder="Enter Email" />
-            <input type="text" placeholder="Username" />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter FullName"
+              value={sign.fullName}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Enter Email"
+              value={sign.email}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Username"
+              name="userName"
+              value={sign.userName}
+              onChange={handleChange}
+            />
 
-            <input type="password" placeholder="password" />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              value={sign.password}
+              onChange={handleChange}
+            />
             <input type="password" placeholder="confirm Password" />
 
             <button type="submit">signUp</button>
